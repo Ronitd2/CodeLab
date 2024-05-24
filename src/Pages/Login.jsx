30966b8e1987a759999ee6d9507d0514aa8c1f76
Ronitd2 <ronitd552@gmail.com>
2024-05-24 16:08:23 +0530

    UI changes

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -19,10 +19,10 @@ export default function Login()
         
             <div className="flex items-center justify-center h-screen w-screen ">
                 <div className="  bg-[#040D12] rounded-3xl p-10 w-[25%] shadow-xl  shadow-gray-900">
-                <h1 className="text-2xl text-center font-semibold">Log In</h1>
+                <h1 className="text-2xl text-center text-gray-400 font-semibold">Log In</h1>
                 <div className="mt-7">
-                <input type="email" onChange={(e)=>{setEmail(e.target.value)}}  className="bg-transparent  border-gray-400 text-gray-500 text-sm  border  border-b-1 border-t-0 border-l-0 border-r-0 focus:ring-blue-500 focus:border-blue-500 block w-[100%] mx-auto  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-500  dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Email"/>
-                <input type="password" onChange={(e)=>{setPassword(e.target.value)}}  className="bg-transparent mt-5 border-gray-400 text-gray-500 text-sm  border  border-b-1 border-t-0 border-l-0 border-r-0 focus:ring-blue-500 focus:border-blue-500 block w-[100%] mx-auto  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-500  dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Password"/>
+                <input type="email" onChange={(e)=>{setEmail(e.target.value)}}  className="bg-transparent  border-gray-400 text-gray-500 text-sm  border  border-b-1 border-t-0 border-l-0 border-r-0 focus:ring-blue-500 focus:border-blue-500 block w-[100%] mx-auto  " placeholder="Email"/>
+                <input type="password" onChange={(e)=>{setPassword(e.target.value)}}  className="bg-transparent mt-5 border-gray-400 text-gray-500 text-sm  border  border-b-1 border-t-0 border-l-0 border-r-0 focus:ring-blue-500 focus:border-blue-500 block w-[100%] mx-auto  " placeholder="Password"/>
 
                 </div>
                 <div className="flex items-center justify-center mt-7">
@@ -39,9 +39,9 @@ export default function Login()
                 : <button onClick={()=>{context.loginfunc(email,password)}} className="bg-[#5755FE] w-32 h-12 rounded-md  p-2 text-white text-md  ">log in</button>}
                 </div>
                 <div className="flex items-center justify-center mt-7">
-                <button onClick={()=>{context.loginwithgoogle()}} className="max-w-70 border-2 border-gray-950 rounded-md p-3">Sign up with google <img className="w-8 h-6  inline-block pl-2" src={googlelogo}></img></button>
+                <button onClick={()=>{context.loginwithgoogle()}} className="max-w-70 border-2 border-gray-950 bg-white rounded-md p-3">Sign up with google <img className="w-8 h-6  inline-block pl-2" src={googlelogo}></img></button>
                 </div>
-                <p className=" text-center text-md font-semibold mt-7">don't have an account ?<Link to="/signup"><span className="text-[#5755FE] text-md font-semibold ml-2">sign up</span></Link></p>
+                <p className=" text-center text-gray-400 text-md font-semibold mt-7">don't have an account ?<Link to="/signup"><span className="text-[#5755FE] text-md font-semibold ml-2">sign up</span></Link></p>
                 </div>
 
                  
