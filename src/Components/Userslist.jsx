55a156a70a5a8bc8729d92e0a5ed57401c4341de
Ronitd2 <ronitd552@gmail.com>
2024-08-10 01:30:46 +0530

    Userlist

--- src/Components/Userslist.jsx
+++ src/Components/Userslist.jsx
@@ -10,7 +10,7 @@ return(
 </button>
 
 
-<div id="multi-dropdown" class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700">
+<div id="multi-dropdown" class="z-50 absolute top-0 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700">
     <ul class="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="multiLevelDropdownButton">
       <li>
         <a href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Dashboard</a>
