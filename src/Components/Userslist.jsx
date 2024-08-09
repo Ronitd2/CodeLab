f08c5f26aafde2fc4356769b0be81ffe5a7fa91a
Ronitd2 <ronitd552@gmail.com>
2024-08-10 03:03:14 +0530

    Userlist

--- src/Components/Userslist.jsx
+++ src/Components/Userslist.jsx
@@ -1,23 +1,40 @@
 import React from "react"
-import { useState } from "react";
+import { useState , useEffect } from "react";
+import SocketContext from '../Context/SocketContext';
 export default function Userlist()
 {
 
  const [play,setPlay]=useState(false);
+ const [users,setUsers]=useState([]);
+ const context=useContext(SocketContext);
+ const socket=context.socket;
+ useEffect(()=>{
+    socket.on("receivedusers", (data) => {
+         setUsers([...users,data]);
+      });
+ },[socket])
 return(    
     <div>
 
-        <button  onClick={()=>{setPlay(!play)}} className="text-white text-md cursor-pointer hover:bg-[#8576FF] hover:rounded-xl p-1">
+        <button  onClick={()=>{setPlay(!play)}} className="text-white text-md cursor-pointer bg-[#8576FF] hover:rounded-xl p-1">
             All Users 
         </button>
 
         {play===true && 
 
-        <div className="z-50 absolute w-24 h-24 bg-slate-700 text-white font-semibold">
-            <p>User 1</p>
-            <p>User 2</p>
-            <p>User 3</p>
-            <p>User 4</p>
+        <div className="z-50 absolute w-24 h-24 bg-slate-700 text-white font-semibold p-1">
+            {
+                users && 
+                users.map((item) => (
+
+                    <div key={item._id} className="flex justify-between gap-1 p-1">
+                       <p>{item.name}</p>
+                       <p>{item.access}</p>
+                       {isadmin===true && <p>remove access</p>}
+                    </div>
+                    
+                ))
+            }
         </div>
 
         }
